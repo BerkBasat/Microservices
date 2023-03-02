90892b3c5450a9ffb9f2d4181c9ba7c1a3ce37cd
BerkBasat <berk.basat@bilgeadamakademi.com>
2023-03-02 14:38:31 +0300

    .

---
+++ Microservices-Weather/CloudWeather.Precipitation/Program.cs
@@ -0,0 +1,40 @@
+using CloudWeather.Precipitation.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<PrecipDbContext>(
+    opts =>
+    {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+);
+
+var app = builder.Build();
+
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) =>
+{
+    if (days == null || days < 1 || days > 30)
+        return Results.BadRequest("Please provide a 'days' query parameter between 1 and 30.");
+
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.Precipitations
+    .Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate)
+    .ToListAsync();
+
+    return Results.Ok(results);
+
+});
+
+app.MapPost("/observation", async (Precipitation precipitation, PrecipDbContext db) =>
+{
+    precipitation.CreatedOn = precipitation.CreatedOn.ToUniversalTime();
+    await db.AddAsync(precipitation);
+    await db.SaveChangesAsync();
+});
+
+app.Run();
